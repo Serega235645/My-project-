0177054cdd7992ec7f0c6631b98cfcb1d11de7e0
Serega235645 <87755400+Serega235645@users.noreply.github.com>
2022-12-21 12:08:31 +0200

    Add files via upload

---
+++ Program2(indexOfChar).cs
@@ -0,0 +1,6 @@
+            Console.WriteLine("Введите строку:");
+            string line = Console.ReadLine();
+            Console.WriteLine(line[line.Length - 1]); 
+            int indexOfChar = (line.Length - 1);
+
+            Console.WriteLine(indexOfChar);
